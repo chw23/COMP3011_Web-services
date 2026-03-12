43b175e1ed687b8a2f5672a94efdee8d9d321bfb
Cheuk Ho Wong <cheukhowong@Tommys-brain.local>
2026-03-12 18:20:50 +0000

    Configure project for PythonAnywhere deployment (whitenoise, STATIC_ROOT)

--- coffee_api/settings.py
+++ coffee_api/settings.py
@@ -31,6 +31,10 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
+# Allow all PythonAnywhere subdomains by default
+if not DEBUG:
+    ALLOWED_HOSTS += ['.pythonanywhere.com']
+
 
 # Application definition
 
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
